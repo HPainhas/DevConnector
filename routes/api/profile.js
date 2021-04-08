4eefbb9d7517e35367750f0df34b362222ea052e
HPainhas <hpainhas@hotmail.com>
2021-04-08 19:44:10 -0400

    Get, create and update profile routes.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,116 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const { body, validationResult } = require('express-validator');
 
-// @route   GET api/profile
-// @desc    Test route
-// @access  Public
-router.get('/', (req, res) => res.send('Profile route'));
+// Bring in normalize to give us a proper url, regardless of what user entered
+const normalize = require('normalize-url');
+
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+const { compareSync } = require('bcryptjs');
+
+// @route   GET api/profile/user
+// @desc    Get current user's profile
+// @access  Private
+router.get('/user', auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({
+            user: req.user.id,
+        }).populate('user', ['name', 'avatar']);
+
+        if (!profile) {
+            return res
+                .status(400)
+                .json({ msg: 'There is no profile for this user' });
+        }
+
+        res.json(profile);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+// @route   POST api/profile
+// @desc    Create or update user's profile
+// @access  Private
+router.post(
+    '/',
+    [
+        auth,
+        [
+            body('status', 'Status is required').not().isEmpty(),
+            body('skills', 'Skills is required').not().isEmpty(),
+        ],
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const {
+            website,
+            skills,
+            youtube,
+            twitter,
+            instagram,
+            linkedin,
+            facebook,
+            // Spread the rest of the fields we don't need to check
+            ...rest
+        } = req.body;
+
+        // Build a profile
+        const profileFields = {
+            user: req.user.id,
+            website:
+                website && website !== ''
+                    ? normalize(website, { forceHttps: true })
+                    : '',
+            skills: Array.isArray(skills)
+                ? skills
+                : skills.split(',').map(skill => ' ' + skill.trim()),
+            ...rest,
+        };
+
+        // Build socialFields object
+        const socialFields = {
+            youtube,
+            twitter,
+            instagram,
+            linkedin,
+            facebook,
+        };
+
+        // Normalize social fields to ensure valid url
+        for (const [key, value] of Object.entries(socialFields)) {
+            if (value && value.length > 0)
+                socialFields[key] = normalize(value, { forceHttps: true });
+        }
+        // Add to profileFields
+        profileFields.social = socialFields;
+
+        try {
+            // Using upsert option (creates new doc if no match is found):
+            let profile = await Profile.findOneAndUpdate(
+                { user: req.user.id },
+                { $set: profileFields },
+                {
+                    new: true,
+                    upsert: true,
+                    setDefaultsOnInsert: true,
+                    useFindAndModify: false,
+                }
+            );
+            return res.json(profile);
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).send('Server Error');
+        }
+    }
+);
 
 module.exports = router;
