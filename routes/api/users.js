24ae015a31df5c9edd5798cf48f06d1468868bb0
HPainhas <hpainhas@hotmail.com>
2021-04-08 18:08:19 -0400

    Implemented JWT.

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { body, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
@@ -56,9 +58,22 @@ router.post(
 
             await user.save();
 
-            // Return jsonwebtoken
+            const payload = {
+                user: {
+                    id: user.id,
+                },
+            };
 
-            res.send('User registered');
+            // Change expiresIn to 3600 (1 hour) before deploying to production
+            jwt.sign(
+                payload,
+                config.get('jwtSecret'),
+                { expiresIn: 360000 },
+                (error, token) => {
+                    if (error) throw error;
+                    res.json({ token });
+                }
+            );
         } catch (error) {
             console.error(error.message);
             res.status(500).send('Server error');
